8fef86aac055d15efbd822fb6e7168cac40b9158
DDeenis <ddeenis46@gmail.com>
2022-11-07 19:01:14 +0200

    minimize to tray

--- MystatDesktopWpf/MainWindow.xaml.cs
+++ MystatDesktopWpf/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Shapes;
 using MaterialDesignThemes.Wpf;
 using MystatDesktopWpf.Services;
 using MystatDesktopWpf.Domain;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace MystatDesktopWpf
 {
@@ -25,6 +26,9 @@ namespace MystatDesktopWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        TaskbarIcon trayIcon;
+        bool realAppClose = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -41,11 +45,85 @@ namespace MystatDesktopWpf
 
             theme.ColorAdjustment = adjustment;
             helper.SetTheme(theme);
+
+            // notification icon initialization
+            trayIcon = new TaskbarIcon()
+            {
+                ToolTipText = "Mystat Desktop",
+                IconSource = new BitmapImage(new Uri("pack://application:,,,/Resources/favicon.ico")),
+                Visibility = Visibility.Hidden,
+                ContextMenu = FindResource("trayIconContextMenu") as ContextMenu
+            };
+            trayIcon.MouseDown += (_, _) => Show();
+            trayIcon.DoubleClickCommand = new TrayDoubleClickCommand(() => MenuItem_Click_1(null, null));
+
+            // on minimize
+            StateChanged += OnStateChanged;
         }
 
         private void window_Closed(object sender, EventArgs e)
         {
+            trayIcon.Dispose();
             SettingsService.Save();
+        }
+
+        private void OnStateChanged(object? sender, EventArgs e)
+        {
+            switch (WindowState)
+            {
+                case WindowState.Minimized:
+                    Hide();
+                    trayIcon.Visibility = Visibility.Visible;
+                    break;
+                default:
+                    trayIcon.Visibility = Visibility.Collapsed;
+                    break;
+            }
+        }
+
+        private void window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+            e.Cancel = !realAppClose;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            realAppClose = true;
+            Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Show();
+            WindowState = WindowState.Normal;
+
+            Topmost = true;
+            Topmost = false;
+
+            Focus();
+        }
+    }
+
+    internal class TrayDoubleClickCommand : ICommand
+    {
+        public Action Callback { get; }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public TrayDoubleClickCommand(Action callback)
+        {
+            Callback = callback;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            Callback();
         }
     }
 }
