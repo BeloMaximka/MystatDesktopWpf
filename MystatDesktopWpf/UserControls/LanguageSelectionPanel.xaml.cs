8128b865bf311506bd4e0c9bf12e5e73b95253a2
BeloMaximka <belomaxim@gmail.com>
2022-11-14 15:46:40 +0200

    lang combobox in login menu

---
+++ MystatDesktopWpf/UserControls/LanguageSelectionPanel.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MystatDesktopWpf.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LanguageSelectionPanel.xaml
+    /// </summary>
+    public partial class LanguageSelectionPanel : UserControl
+    {
+        public LanguageSelectionPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
