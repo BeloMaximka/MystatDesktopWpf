8ab49ec19c72b83b9500ba7bbb8150692beec9e1
BeloMaximka <belomaxim@gmail.com>
2022-11-14 13:50:01 +0200

    better language combobox

---
+++ MystatDesktopWpf/Converters/ToUpperConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MystatDesktopWpf.Converters
+{
+    public class ToUpperConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string;
+            return string.IsNullOrEmpty(str) ? string.Empty : str.ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string;
+            return string.IsNullOrEmpty(str) ? string.Empty : str.ToLower();
+        }
+    }
+}
---
+++ MystatDesktopWpf/ViewModels/LanguageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MystatDesktopWpf.ViewModels
+{
+    internal class LanguageViewModel : ViewModelBase
+    {
+        public LanguageViewModel()
+        {
+            index = Languages.IndexOf(App.Language);
+        }
+        public List<CultureInfo> Languages => App.Languages;
+
+        int index;
+        public int SelectedIndex
+        {
+            get => index;
+            set
+            {
+                index = value;
+                App.Language = App.Languages[index];
+                OnPropertyChanged();
+                OnPropertyChanged("SelectedLanguageName");
+            }
+        }
+
+        public string SelectedLanguageName { get => App.Language.TwoLetterISOLanguageName.ToUpper(); }
+    }
+}
