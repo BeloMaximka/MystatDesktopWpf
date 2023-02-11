fcb6586baaae0e0d246335f4eccf1051af0a8f27
DDeenis <ddeenis46@gmail.com>
2023-02-11 20:08:13 +0000

    add new entry to settings

---
+++ MystatDesktopWpf/SubSettings/TimezoneSubSettings.cs
@@ -0,0 +1,41 @@
+﻿using MystatDesktopWpf.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MystatDesktopWpf.SubSettings
+{
+    internal class TimezoneSubSettings : ISettingsProperty
+    {
+        public event Action? OnPropertyChanged;
+
+        TimeZoneInfo from = TimeZoneInfo.Local;
+        TimeZoneInfo to = TimeZoneInfo.Local;
+
+        public TimeZoneInfo From
+        {
+            get => from;
+            set
+            {
+                from = value;
+                PropertyChanged();
+            }
+        }
+        public TimeZoneInfo To
+        {
+            get => to;
+            set
+            {
+                to = value;
+                PropertyChanged();
+            }
+        }
+
+        public void PropertyChanged()
+        {
+            OnPropertyChanged?.Invoke();
+        }
+    }
+}
---
+++ MystatDesktopWpf/ViewModels/TimezoneSettingsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using MystatDesktopWpf.Services;
+using MystatDesktopWpf.SubSettings;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MystatDesktopWpf.ViewModels
+{
+    internal class TimezoneSettingsViewModel : ViewModelBase
+    {
+        private readonly TimezoneSubSettings timezoneSubSettings;
+
+        public ReadOnlyCollection<TimeZoneInfo> TimeZones { get; set; }
+        public TimeZoneInfo TimezoneFrom 
+        { 
+            get => timezoneSubSettings.From;
+            set
+            {
+                timezoneSubSettings.From = value;
+                OnPropertyChanged(nameof(TimezoneFrom));
+            }
+        }public TimeZoneInfo TimezoneTo 
+        { 
+            get => timezoneSubSettings.From;
+            set
+            {
+                timezoneSubSettings.To = value;
+                OnPropertyChanged(nameof(TimezoneTo));
+            }
+        }
+
+        public TimezoneSettingsViewModel()
+        {
+            TimeZones = TimeZoneInfo.GetSystemTimeZones();
+            timezoneSubSettings = SettingsService.Settings.TimezoneConvertion;
+        }
+    }
+}
