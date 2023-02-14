4912802c50cf26dd39b353a17408630a0612e921
BeloMaximka <belomaxim@gmail.com>
2023-02-14 21:00:05 +0200

    basic evaluation lesson

---
+++ MystatDesktopWpf/UserControls/Menus/LessonEvaluation.xaml.cs
@@ -0,0 +1,80 @@
+﻿using MaterialDesignThemes.Wpf;
+using MystatDesktopWpf.Domain;
+using MystatDesktopWpf.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MystatDesktopWpf.UserControls.Menus
+{
+    /// <summary>
+    /// Interaction logic for LessonEvaluation.xaml
+    /// </summary>
+    public partial class LessonEvaluation : UserControl, IRefreshable
+    {
+        private readonly LessonEvaluationViewModel viewModel = new();
+        public LessonEvaluation()
+        {
+            DataContext = viewModel;
+
+            InitializeComponent();
+            Refresh();
+        }
+
+        public async void Refresh()
+        {
+            if (viewModel.LoadingLessons) return;
+            LoadingProgress.Visibility = Visibility.Visible;
+            await viewModel.LoadLessons();
+            LoadingProgress.Visibility = Visibility.Collapsed;
+        }
+
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button &&
+                button.Tag is EvaluateLessonItemWithMark item &&
+                button.Parent is StackPanel panel &&
+                panel.FindName("ErrorText") is TextBlock errorText)
+            {
+                if (item.LessonMark == 0 || item.TeacherMark == 0)
+                {
+                    string homeworkDownloadError = (string)FindResource("m_HomeworkDownloadError");
+                    errorText.Text = homeworkDownloadError;
+                    errorText.Visibility = Visibility.Visible;
+                    return;
+                }
+
+                errorText.Visibility = Visibility.Collapsed;
+                return; // temp
+                ButtonProgressAssist.SetIsIndicatorVisible(button, true);
+                try
+                {
+                    await viewModel.EvaluateLesson(item);
+                }
+                catch
+                {
+                    string homeworkDownloadError = (string)FindResource("m_HomeworkDownloadError");
+                    errorText.Text = homeworkDownloadError;
+                    errorText.Visibility = Visibility.Visible;
+
+                }
+                ButtonProgressAssist.SetIsIndicatorVisible(button, false);
+            }
+        }
+
+        private void SendWithMaxMarksButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is EvaluateLessonItemWithMark item)
+            {
+                if (button.Parent is FrameworkElement element &&
+                   element.Parent is FrameworkElement parent &&
+                   parent.FindName("LessonRatingBar") is RatingBar lessonRatingBar &&
+                   parent.FindName("TeacherRatingBar") is RatingBar teacherRatingBar)
+                {
+                    lessonRatingBar.Value = 5;
+                    teacherRatingBar.Value = 5;
+                }
+                SendButton_Click(sender, e);
+            }
+        }
+    }
+}
---
+++ MystatDesktopWpf/ViewModels/LessonEvaluationViewModel.cs
@@ -0,0 +1,63 @@
+﻿using MystatAPI.Entity;
+using MystatDesktopWpf.Domain;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace MystatDesktopWpf.ViewModels
+{
+    internal class LessonEvaluationViewModel : ViewModelBase
+    {
+        private ObservableCollection<EvaluateLessonItemWithMark> lessons = new();
+        public ObservableCollection<EvaluateLessonItemWithMark> Lessons { get => lessons; set => SetProperty(ref lessons, value); }
+
+        public bool LoadingLessons { get; private set; }
+        public async Task LoadLessons()
+        {
+            if (LoadingLessons) return;
+            LoadingLessons = true;
+            RetryDelayProvider delay = new();
+            while (true)
+            {
+                try
+                {
+                    var result = await MystatAPISingleton.Client.GetEvaluateLessonList();
+                    List<EvaluateLessonItemWithMark> lessons = new();
+                    foreach (var item in result)
+                    {
+                        lessons.Add(new EvaluateLessonItemWithMark(item));
+                    }
+                    Lessons = new(lessons);
+                    break;
+                }
+                catch
+                {
+                    await Task.Delay(delay.ProvideValueMilliseconds());
+                }
+            }
+            LoadingLessons = false;
+        }
+
+        public async Task EvaluateLesson(EvaluateLessonItemWithMark item)
+        {
+            if (await MystatAPISingleton.Client.EvaluateLesson(item.Key, item.LessonMark, item.TeacherMark, item.Comment))
+            {
+                Lessons.Remove(item);
+            }
+        }
+    }
+
+    internal class EvaluateLessonItemWithMark : EvaluateLessonItem
+    {
+        public EvaluateLessonItemWithMark(EvaluateLessonItem item)
+        {
+            VisitDate = item.VisitDate;
+            TeacherFullName = item.TeacherFullName;
+            Key = item.Key;
+            SpecName = item.SpecName;
+        }
+        public int LessonMark { get; set; } = 0;
+        public int TeacherMark { get; set; } = 0;
+        public string? Comment { get; set; }
+    }
+}
