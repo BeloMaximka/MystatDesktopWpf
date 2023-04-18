a34ea668a2b5778fd0afc69b50fc6c7432eefd07
BeloMaximka <belomaxim@gmail.com>
2023-04-18 21:49:14 +0300

    long login fix

--- MystatDesktopWpf/Domain/MystatAPISingleton.cs
+++ MystatDesktopWpf/Domain/MystatAPISingleton.cs
@@ -7,14 +7,17 @@ namespace MystatDesktopWpf.Domain
     internal static class MystatAPISingleton
     {
         static public MystatAPIClient Client { get; private set; }
-        static public ProfileInfo? Profile { get; private set; }
+        static public Task<ProfileInfo> DeferredProfileInfo { get; private set; }
         static async public Task<MystatAuthResponse> LoginAndGetProfileInfo()
         {
             var result = await Client.Login();
             if (result is MystatAuthSuccess responseSuccess)
-                Profile = await Client.GetProfileInfo();
+            {
+				DeferredProfileInfo = Client.GetProfileInfo();
+			}
             return result;
         }
+
         static MystatAPISingleton()
         {
             Client = new MystatAPIClient();
